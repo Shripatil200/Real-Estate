9b8e57e8b80767372eb7f9a629e8eedd287fe9d4
Shripatil200 <patilshriram200@gmail.com>
2025-09-09 23:14:20 +0530

    HousingLocation interface updated

--- src/app/services/housing-location.ts
+++ src/app/services/housing-location.ts
@@ -1,2 +1,10 @@
 export interface HousingLocation {
+    id: Number,
+    name: String,
+    city: String,
+    state: String,
+    photo: String,
+    availableUnits: Number,
+    wifi: boolean,
+    laundry: boolean,
 }
