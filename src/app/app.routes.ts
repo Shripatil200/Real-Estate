c2d595e6023b01af327278e0c470d56913f4bd90
Shripatil200 <patilshriram200@gmail.com>
2025-09-09 23:00:14 +0530

    logo added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home-component/home-component';
+import { DetailsComponent } from './components/details-component/details-component';
+import { PageNotFoundComponent } from './page-not-found.component/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'/home', pathMatch:'full'},
+
+    {path:'home' , component:HomeComponent},
+    {path:'details', component:DetailsComponent},
+
+
+
+    
+    {path:"**", component:PageNotFoundComponent},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
