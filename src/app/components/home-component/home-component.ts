9f17fae67002b166306008b49d74b516e285cc9d
Shripatil200 <patilshriram200@gmail.com>
2025-09-10 00:31:20 +0530

    HomeComponent - search filter added

--- src/app/components/home-component/home-component.ts
+++ src/app/components/home-component/home-component.ts
@@ -33,7 +33,7 @@ export class HomeComponent {
     }
 
     this.filteredLocationList = this.housingLocationList.filter((housingLocation)=>{
-      housingLocation?.city.toLowerCase().includes(text.toLowerCase());
+      return housingLocation?.city.toLowerCase().includes(text.toLowerCase());
     })
   }
 
